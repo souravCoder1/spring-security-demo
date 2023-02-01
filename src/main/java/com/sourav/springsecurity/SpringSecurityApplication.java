b85f347c81da9fff24e5b3e30bf4d184d3c1cd33
souravpal01 <souravpal7217@gmail.com>
2023-02-01 16:02:58 +0530

    code change

--- src/main/java/com/sourav/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/sourav/springsecurity/SpringSecurityApplication.java
@@ -1,13 +1,10 @@
 package com.sourav.springsecurity;
 
-import com.sourav.springsecurity.jwt.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableConfigurationProperties(JwtConfig.class)
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/sourav/springsecurity/config/SpringSecOAUTHGitHubConfig.java
@@ -0,0 +1,70 @@
+package com.sourav.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecOAUTHGitHubConfig {
+
+    /**
+     * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
+     * to move towards a component-based security configuration. It is recommended to create a bean
+     * of type SecurityFilterChain for security related configurations.
+     *
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> {
+            try {
+                auth.anyRequest()
+                        .authenticated()
+                        .and()
+                        .oauth2Login();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            };
+        });
+        return http.build();
+
+    }
+
+//    private ClientRegistration clientRegistration() {
+//        return CommonOAuth2Provider
+//                .GITHUB.getBuilder("github")
+//                .clientId("0f43114c9178bba36981")
+//                .clientSecret("b18b21403cf18f53708ad441b173c508271ad9d1").build();
+//    }
+
+
+    /*private ClientRegistration clientRegistration1() {
+        ClientRegistration cr =
+                ClientRegistration.withRegistrationId("github")
+                        .clientId("3c9be97074f067e78e75")
+                        .clientSecret("ab313f7ade3d79e06c192ca80cf152c43cb5d916")
+                        .scope(new String[]{"read:user"})
+                        .authorizationUri("https://github.com/login/oauth/authorize")
+                        .tokenUri("https://github.com/login/oauth/access_token")
+                        .userInfoUri("https://api.github.com/user")
+                        .userNameAttributeName("id").clientName("OAuth2GITHUB")
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .redirectUriTemplate("{baseUrl}/{action}/oauth2/code/{registrationId}")
+                        .build();
+        return cr;
+    }*/
+
+
+//    @Bean
+//    public ClientRegistrationRepository clientRepository1() {
+//        ClientRegistration clientReg = clientRegistration();
+//        return new
+//                InMemoryClientRegistrationRepository(clientReg);
+//    }
+
+
+}
